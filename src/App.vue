968baf4f2df73dfbf53d174f2d12827616a9b734
吉他 <3301828908@qq.com>
2025-09-08 21:38:28 +0800

    updata

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box"></div>
+</template>
+
+<script setup lang="ts">
+let a = 1
+let b = 10
+if (a === b) {
+  console.log()
+}
+</script>
+
+<style scoped lang="scss">
+</style>
